9cf077e8fa8264384e26fe68510f13f32bde206d
neyaznafiz <niiyazobug@gmail.com>
2022-08-01 04:20:12 +0600

    contect added in purchase event component

---
+++ src/Components/Home/PurchaseEvent/PurchaseEvent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function PurchaseEvent() {
+    return (
+        <div className=" p-3 bg-image"
+        style={{ backgroundImage: "url('https://i.ibb.co/DLQD5bZ/special-offer-bg.png')", backgroundSize: 'cover' }} >
+
+            <div>
+                <p>
+                    <span>Are You Ready To Make</span>
+                    <span>Your Own Special Events?</span>
+                </p>
+                <p>“Get started now, Harmoni event management PSD template.”</p>
+            </div>
+
+            <div>
+                <button>purchase now</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default PurchaseEvent
