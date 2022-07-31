291d1470eab7a9e32dfca8dcbe0e4dc164c477c9
MK-Sohan <mksohan786@gmail.com>
2022-08-01 03:03:40 +0600

    carusel

--- src/Components/Home/UpcommitngEvent/UpcommingEvent.js
+++ src/Components/Home/UpcommitngEvent/UpcommingEvent.js
@@ -36,6 +36,32 @@ const UpcommingEvent = () => {
     slidesToShow: 2,
     centerMode: true,
     centerPadding: 270,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     beforeChange: (current, next) => setImageIndex(next),
