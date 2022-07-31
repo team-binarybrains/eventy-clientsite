9b120a00c1bfb40dfe99326391d83a4343d8370c
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-07-31 22:29:42 +0600

    book btn placement adjusted

--- src/Components/Home/Venues/Venues.js
+++ src/Components/Home/Venues/Venues.js
@@ -18,8 +18,8 @@ const Venues = () => {
     }
 
     return (
-        <div className='bg-[#333333] py-[100px] flex sm:flex-row flex-col sm:items-end sm:justify-center gap-5'>
-            <div className='grow basis-[50%] shrink px-3 sm:px-0'>
+        <div className='bg-[#333333] py-[100px] flex sm:flex-row flex-col sm:items-end sm:justify-center gap-x-[1%] gap-y-5 flex-wrap'>
+            <div className='basis-[49%] shrink px-3'>
                 <div className='max-w-[530px] ml-auto text-center sm:text-left'>
                     <div className='h-[3px] w-[70px] bg-gradient-to-r from-red-500 to-pink-500 mx-auto sm:mx-0 mb-2' />
                     <h4 className={`${styles.openSans} text-white uppercase tracking-[5px] mb-4`}>Harmoni Venues</h4>
@@ -29,7 +29,7 @@ const Venues = () => {
 
                     {
                         venues.map((venue)=> {
-                            return <div className={`h-[141px] max-w-[424.4px] mr-[30px] p-[30px] flex items-start gap-[10%] text-white whitespace-pre cursor-pointer ${venue.id===select.id && styles.selected}`} key={venue.id} onClick={() => selection(venue)}>
+                            return <div className={`h-[141px] max-w-[424.4px] mr-[30px] p-[30px] flex items-start gap-[10%] text-white whitespace-pre cursor-pointer ${venue.id===select.id && styles.selected} `} key={venue.id} onClick={() => selection(venue)}>
                                 <img className='h-[80px] max-w-[80px] object-cover overflow-hidden' src={venue?.img} alt="" />
                                 <div className='space-y-1 shrink'>
                                     <h5 className=''><span className='text-red-600 font-bold'>{`${venue?.star} ⭐ ${venue?.venueName}`}</span> Hotel</h5>
@@ -43,11 +43,11 @@ const Venues = () => {
                 </div>
             </div>
             <div className={`h-[424px] sm:min-w-[500px] grow basis-[50%] shrink relative ${styles.highlight}`}>
-                <img src={select.img} alt="" className={`w-full h-[200px] sm:h-[424px] object-cover ${styles.child} opacity-80`}/>
+                <img src={select.img} alt="" className={`w-full h-[200px] sm:h-[424px] object-cover ${styles.child} opacity-80 `}/>
                 <div className={`absolute top-0 left-0 ${styles.batch} flex justify-end pr-6`}>
                     <p className='font-bold text-lg text-center rotate-90'>{select.star} ⭐ <br /> Hotel</p>
                 </div>
-                <button className={`absolute bg-gradient-to-r from-red-500 to-pink-500 top-[48%] right-[41%] px-5 py-2 pt-3 rounded-full text-white font-bold uppercase z-10 hover:scale-105 transition-transform active:scale-100`}>Book Now</button>
+                <button className={`absolute bg-gradient-to-r from-red-500 to-pink-500 top-[calc(50%-25px)] right-[calc(50%-69px)] px-5 py-2 pt-3 rounded-full text-white font-bold uppercase z-10 hover:scale-105 transition-transform active:scale-100`}>Book Now</button>
             </div>
         </div>
     );
