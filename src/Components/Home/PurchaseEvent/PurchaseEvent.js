eecb76eea5292c432d2bbcef8342651f636b4077
neyaznafiz <niiyazobug@gmail.com>
2022-08-01 04:43:13 +0600

    puschase section style done

--- src/Components/Home/PurchaseEvent/PurchaseEvent.js
+++ src/Components/Home/PurchaseEvent/PurchaseEvent.js
@@ -5,17 +5,17 @@ function PurchaseEvent() {
         <div className=" p-3 bg-image"
             style={{ backgroundImage: "url('https://i.ibb.co/DLQD5bZ/special-offer-bg.png')", backgroundSize: 'cover' }} >
 
-            <div>
-                <div className='text-center'>
-                    <p className='text-3xl text-white uppercase'>
+            <div className='my-16'>
+                <div className='text-center -mt-8'>
+                    <p className='lg:text-3xl text-xl text-white uppercase'>
                         <span>Are You Ready To Make</span>
-                        <span className='font-bold pl-2'>Your Own Special Events?</span>
+                        <span className='font-bold pl-2'>Your Own Special Events <span className='lg:text-7xl'>?</span></span>
                     </p>
-                    <p>“Get started now, Harmoni event management PSD template.”</p>
+                    <p className='my-2'>“Get started now, Harmoni event management PSD template.”</p>
                 </div>
 
-                <div className='flex justify-center'>
-                    <button>purchase now</button>
+                <div className='flex justify-center lg:mt-12 mt-5 lg:-mb-7 -mb-9'>
+                    <button className='bg-white text-rose-600 lg:px-8 px-4 lg:py-4 py-3 uppercase font-bold rounded-full tracking-wide'>purchase now!</button>
                 </div>
             </div>
 
