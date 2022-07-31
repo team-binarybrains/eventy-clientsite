10ae829fd2f8963b3600b26760d15b09adc26ac3
MK-Sohan <mksohan786@gmail.com>
2022-08-01 02:07:17 +0600

    blog section design

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Blogs from "./Blog/Blogs";
 import EventListing from "./EventListing/EventListing";
 
 const Home = () => {
   return (
     <div>
       <EventListing></EventListing>
+      <Blogs></Blogs>
     </div>
   );
 };
