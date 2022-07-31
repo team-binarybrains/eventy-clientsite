90de46149e0260d3efe07416109b669875d4310b
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-08-01 04:52:53 +0600

    offer don

--- src/Components/Home/Offer/Offer.js
+++ src/Components/Home/Offer/Offer.js
@@ -3,12 +3,12 @@ import './Offer.css'
 const Offer = () => {
     return (
         <div className="backgroundImage">
-          <div className="flex justify-around py-24">
+          <div className="md:flex justify-around items-center py-10">
             <div>
-                <h1 className=""><span className="text-7xl font-bold text-[#fd1d1d]">30%</span> <span className="text-4xl text-white">Off In June~July</span> <span className="text-4xl font-bold text-white">For Birthday Events</span></h1>
-                <p className="py-5 text[#333333] text-lg">Contact us now and we will make your event unique & unforgettabl</p>
+                <h1 className=" tracking-widest"><span className="text-7xl font-black px-1 text-[#fd1d1d]">30%</span> <span className="text-4xl text-white">Off In June~July</span> <span className="text-4xl  text-white font-black">For Birthday Events</span></h1>
+                <p className="py-5 text[#333333] text-lg tracking-widest font-bold">Contact us now and we will make your event unique & unforgettabl</p>
             </div>
-           <button className=" font-bold bg-white  hover:text-white hover:bg-[#fd1d1d] px-16 rounded-lg">make an event now</button>
+           <button className=" font-bold bg-white  hover:text-white hover:bg-[#fd1d1d] px-16 rounded-full py-5 text-lg">make an event now</button>
           </div>
         </div>
     );
