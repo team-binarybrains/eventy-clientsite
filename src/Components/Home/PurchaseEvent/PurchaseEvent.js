969c2c54d644b4eb45b48a4bedd9ccccd5714aeb
neyaznafiz <niiyazobug@gmail.com>
2022-08-01 04:25:04 +0600

    text align impliment

--- src/Components/Home/PurchaseEvent/PurchaseEvent.js
+++ src/Components/Home/PurchaseEvent/PurchaseEvent.js
@@ -3,20 +3,22 @@ import React from 'react'
 function PurchaseEvent() {
     return (
         <div className=" p-3 bg-image"
-        style={{ backgroundImage: "url('https://i.ibb.co/DLQD5bZ/special-offer-bg.png')", backgroundSize: 'cover' }} >
+            style={{ backgroundImage: "url('https://i.ibb.co/DLQD5bZ/special-offer-bg.png')", backgroundSize: 'cover' }} >
 
             <div>
-                <p>
-                    <span>Are You Ready To Make</span>
-                    <span>Your Own Special Events?</span>
-                </p>
-                <p>“Get started now, Harmoni event management PSD template.”</p>
+                <div className='text-center'>
+                    <p className='text-3xl text-white uppercase'>
+                        <span>Are You Ready To Make</span>
+                        <span className='font-bold pl-2'>Your Own Special Events?</span>
+                    </p>
+                    <p>“Get started now, Harmoni event management PSD template.”</p>
+                </div>
+
+                <div className='flex justify-center'>
+                    <button>purchase now</button>
+                </div>
             </div>
 
-            <div>
-                <button>purchase now</button>
-            </div>
-            
         </div>
     )
 }
