eeb6dc2d88b81f7ea02d68b90aa2e298ca1c29b7
neyaznafiz <niiyazobug@gmail.com>
2022-07-31 18:46:37 +0600

    title added of about

---
+++ src/Components/Home/AboutUs/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function AboutUs() {
+    return (
+        <div>
+            <div>
+            <h1 className='uppercase my-5 text-gray-400 text-[20px] tracking-[10px]'>we are eventy</h1>
+            <h1 className='text-4xl font-bold'>A Great Events Management</h1>
+            
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AboutUs from "./AboutUs/AboutUs";
 import Gallery from "./Gallery/Gallery";
 
 const Home = () => {
   return (
     <div>
       <Gallery></Gallery>
+      <AboutUs></AboutUs>
     </div>
   );
 };
