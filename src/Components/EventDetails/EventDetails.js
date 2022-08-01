341b317948c4fc543e8ba654b9be32c0ddd57636
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-08-01 21:25:42 +0600

    branch created

---
+++ src/Components/EventDetails/EventDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './EventDetails.module.css'
+
+const EventDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EventDetails;
