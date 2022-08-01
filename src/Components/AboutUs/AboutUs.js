25de0a4cc9bc888d91dd13e35971d3c2d34ab569
neyaznafiz <niiyazobug@gmail.com>
2022-08-01 22:15:52 +0600

    bg img added in about us

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function AboutUs() {
     return (
-        <div>
-            
+        <div className=" p-3 bg-image h-[350px]"
+            style={{ backgroundImage: "url('https://i.ibb.co/LQr53RB/1-testimonial-bg-edited.jpg')", backgroundSize: 'cover',  }} >
+
+            <div>
+
+            </div>
+
         </div>
     )
 }
