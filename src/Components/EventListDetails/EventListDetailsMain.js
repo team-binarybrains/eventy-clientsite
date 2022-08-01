7f0d8075ee431e1c781effe65113c3e71d2fa4dd
MK-Sohan <mksohan786@gmail.com>
2022-08-02 01:41:54 +0600

    top bar design

--- src/Components/EventListDetails/EventListDetailsMain.js
+++ src/Components/EventListDetails/EventListDetailsMain.js
@@ -1,10 +1,12 @@
 import React from "react";
+import TopnavBar from "../Share/TopBar/TopnavBar";
 import AllEventList from "./AllEventList";
 import EventListSearchhead from "./EventListSearchhead";
 
 const EventListDetailsMain = () => {
   return (
     <div>
+      <TopnavBar></TopnavBar>
       <EventListSearchhead></EventListSearchhead>
       <AllEventList></AllEventList>
     </div>
