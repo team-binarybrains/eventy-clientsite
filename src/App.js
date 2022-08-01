3ea85ad46f7882406cefd9901b2dbb614c83fcdc
neyaznafiz <niiyazobug@gmail.com>
2022-08-01 21:57:50 +0600

    about us route add

--- src/App.js
+++ src/App.js
@@ -13,16 +13,15 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 
 import EventListDetails from "./Components/EventListDetails/EventListSearchhead";
 import EventListDetailsMain from "./Components/EventListDetails/EventListDetailsMain";
+import AboutUs from "./Components/AboutUs/AboutUs";
 function App() {
   return (
     <div className="overflow-x-hidden">
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/authentication" element={<Form></Form>}></Route>
-        <Route
-          path="/eventlist"
-          element={<EventListDetailsMain></EventListDetailsMain>}
-        ></Route>
+        <Route path="/eventlist" element={<EventListDetailsMain></EventListDetailsMain>}></Route>
+        <Route path="/about" element={<AboutUs></AboutUs>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <BackTopBtn></BackTopBtn>
---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutUs() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AboutUs
