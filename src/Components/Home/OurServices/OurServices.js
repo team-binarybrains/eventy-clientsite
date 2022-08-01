6777ef6e92712f44a0cecb3e151b57fc263060ae
neyaznafiz <niiyazobug@gmail.com>
2022-08-01 13:28:07 +0600

    component changed

--- src/Components/Home/OurServices/OurServices.js
+++ src/Components/Home/OurServices/OurServices.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function ourServices() {
+function OurServices() {
     return (
         <div>
-            
+
         </div>
     )
 }
 
-export default ourServices
+export default OurServices
