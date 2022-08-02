1c1198c89bbf92f2951914c92aa49e6559a6ed43
abdur-rashid-khan <rashidkhan56925@gmail.com>
2022-08-03 02:37:57 +0600

    n/c

--- src/Components/EventDetails/FeaturedEvent/EventSchedule/DayOne/DayOne.js
+++ src/Components/EventDetails/FeaturedEvent/EventSchedule/DayOne/DayOne.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaMicrophone,FaTicketAlt,FaRegClock } from 'react-icons/fa';
+import dayOne from '../../img/1.hall.png'
 
 
 const DayOne = () => {
@@ -11,33 +12,33 @@ const DayOne = () => {
                <p className='text-slate-700 py-4'>Lorem ipsum dollor site amet the best consectuer diam nerd adipiscing elites sed diam nonummy nibh the ebest uismod tincidunt ut laoreet dolore magna aliquam erat volutpat guild insignia the consectuer adipiscing elit.</p>
             </div>
             <div className="">
-               <div className="flex  gap-2">
+               <div className="flex  gap-4">
                   <div className="flex items-center gap-2">
                      <FaMicrophone  className='text-5xl p-3 rounded-full bg-yellow-400'/>
                      <div className="">
-                        <h1 className='text-sm'>Speaker</h1>
-                        <h1 className='text-sm'>JENNY JUIS</h1>
+                        <h1 className='text-base text-slate-600 font-semibold'>Speaker</h1>
+                        <h1 className='text-lg font-semibold text-slate-700'>JENNY JUIS</h1>
                      </div>
                   </div>
                   <div className="flex items-center  gap-2">
-                     <FaTicketAlt />
+                     <FaTicketAlt className='text-5xl p-3 rounded-full bg-yellow-400'/>
                      <div className="">
-                        <h1>Speaker</h1>
+                        <h1 className='text-base text-slate-600 font-semibold'>Speaker</h1>
                         <h1>JENNY JUIS</h1>
                      </div>
                   </div>
                   <div className="flex items-center  gap-2">
-                     <FaRegClock />
+                     <FaRegClock className='text-5xl p-3 rounded-full bg-yellow-400'/>
                      <div className="">
-                        <h1>Speaker</h1>
-                        <h1>JENNY JUIS</h1>
+                        <h1 className='text-base text-slate-600 font-semibold'>Speaker</h1>
+                        <h1 className='text-lg font-semibold text-slate-700'>JENNY JUIS</h1>
                      </div>
                   </div>
                </div>
             </div>
          </div>
          <div className="grow-0 basis-[200px]">
-            <img src="" className='h-96 max-w-sm w-full mx-auto' alt="" />
+            <img src={dayOne} className='h-auto max-w-sm w-full mx-auto' alt="" />
          </div>
       </div>
    );
