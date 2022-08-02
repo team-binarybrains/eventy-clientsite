ba705319d1f7decd0a521f1815da5b70fc30a546
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-08-02 21:42:24 +0600

    style updated in locationfaq.js file

--- src/Components/EventDetails/EventDetails.js
+++ src/Components/EventDetails/EventDetails.js
@@ -14,14 +14,14 @@ const EventDetails = () => {
                     <TopBanner></TopBanner>
                 </div>
 
-                <section className='px-3 flex flex-wrap justify-center'>
+                <section className='px-3 flex flex-wrap justify-center container mx-auto max-w-7xl lg:px-16'>
                     {/* 2nd row : 1st column (-----Rashid vai-------)*/}
-                    <div className='border-4 border-red-600 basis-[50%] grow min-w-[800px] min-h-[200px] shrink'>
+                    <div className='border-4 border-red-600 basis-[800px] shrink max-w-[52rem]'>
                         <FeaturedEvent></FeaturedEvent>
                     </div>
 
                     {/* 2nd row : 2nd column */}
-                    <div className='basis-[30%] grow min-w-[350px] min-h-[200px] shrink'>
+                    <div className='basis-[350px] grow shrink max-w-[59rem]'>
                         <LocationFaq></LocationFaq>
                     </div>
                 </section>
