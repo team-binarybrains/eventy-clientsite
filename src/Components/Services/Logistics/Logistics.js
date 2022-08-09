f6ce2820ba983519574ea4cdfaa9b132f3f43ef7
neyaznafiz <niiyazobug@gmail.com>
2022-08-09 20:39:28 +0600

    information banner impliment

--- src/Components/Services/Logistics/Logistics.js
+++ src/Components/Services/Logistics/Logistics.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useFetch from '../../Home/OurServices/Hook/useFetch'
 
 function Logistics() {
+
+    const logisticServices = useFetch('http://localhost:5000/get-logistic')
+
     return (
         <div className='route'>
             <div className=" p-3 bg-image lg:h-[340px] h-[200px] banner-background">
@@ -26,7 +30,31 @@ function Logistics() {
                 </div>
             </div>
 
+            {/* -------------- */}
 
+            <div className='lg:py-20 bg-gray-100 '>
+                <div className='grid lg:flex justify-center gap-x-6'>
+
+                    <div className='lg:w-4/12 flex justify-center items-center'>
+                        <img src="https://i.ibb.co/BBskSqc/world-tour-2017-highlights.jpg" alt="" className='h-96' />
+                    </div>
+
+                    <div className='lg:w-4/12 px-3 lg:px-0 py-10 lg:py-0'>
+                        <p className='uppercase text-3xl font-semibold'>EVENTY EVENT PLANNING <br />
+                        <span className='text-amber-400 tracking-widest mr-2'>LOGISTICS</span>
+                        AND
+                        <span className='text-amber-400 tracking-widest ml-2'>REGISTRATION</span>
+                        </p>
+
+                        <p className='py-8'>
+                        We plan events in Washington, and nationally in Atlanta, New York, Los Angeles, Miami, Atlanta and Chicago. We manage transportation, venue negotiation, logistics, registration, destination management, hotel rooms, security and offer a customized experience for your organization. Our event planning logistics and registration services can meet the needs of any size conference, convention or meeting. We also handle logistics for social events, fundraisers and political events.
+                        </p>
+                        <div className='flex justify-end'>
+                            <img src="https://i.ibb.co/MRXgQwx/eventy-signature.png" alt="" className='' />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
         </div>
