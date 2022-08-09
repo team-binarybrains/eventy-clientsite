23c14ff0d586eae8f1955ee66bad0e93205e97f9
neyaznafiz <niiyazobug@gmail.com>
2022-08-09 19:23:23 +0600

    information banner impliment in destination component

--- src/Components/Services/Destination/Destination.js
+++ src/Components/Services/Destination/Destination.js
@@ -27,7 +27,28 @@ function Destination() {
             </div>
 
 
+{/* -------------- */}
 
+<div className='lg:py-20 bg-gray-100 '>
+                <div className='grid lg:flex justify-center gap-x-6'>
+
+                    <div className='lg:w-4/12 flex justify-center items-center'>
+                        <img src="https://i.ibb.co/W6x98Gs/bdes.jpg" alt="" className='h-96' />
+                    </div>
+
+                    <div className='lg:w-4/12 px-3 lg:px-0 py-10 lg:py-0'>
+                        <p className='uppercase text-3xl font-semibold'> <span className='text-amber-400 tracking-widest mr-2'>DESTINATION</span>
+                         MANAGEMENT SERVICES
+                        </p>
+                        <p className='py-8'>
+                        Eventy destination Management services include: tourism planning, transportation, PRIVATE security details, hotel accommodation, restaurants, activities, excursions, conference venues, themed events, gala dinners and logistics, meetings, incentive schemes as well as helping with overcoming language barriers. We utilize a structured tour model or a small group luxury tour model that is personal and catered to our corporate executives and dignitaries. All of our tours include welcome gifts and some of the most beloved souvenirs from the venues. To provide unparalleled event management solutions while providing our valued clients with a unique, customized experience that fully captures the essence of Wine Country. We also offer destination design services in the greater San Francisco-Bay Area upon request.
+                        </p>
+                        <div className='flex justify-end'>
+                            <img src="https://i.ibb.co/MRXgQwx/eventy-signature.png" alt="" className='' />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
