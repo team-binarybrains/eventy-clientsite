49815b1cc654633c34164788bd94cfc27a06da42
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-08-09 12:27:35 +0600

    require admin implemented

---
+++ src/Components/Authentication/RequireAdmin/RequireAdmin.js
@@ -0,0 +1,22 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../../Firebase/firebase.init";
+import useAdmin from "../../Hooks/useAdmin";
+import Loading from "../../Share/Loading/Loading";
+
+
+const RequireAdmin = ({ children }) => {
+    const [user, loading] = useAuthState(auth);
+    const [admin, adminLoading] = useAdmin(user);
+    const location = useLocation();
+    if (loading || adminLoading) {
+        return <Loading />;
+    }
+    if (!user || !admin) {
+        return <Navigate to="/authentication" state={{ from: location }} replace />;
+    }
+
+    return children;
+};
+
+export default RequireAdmin;
