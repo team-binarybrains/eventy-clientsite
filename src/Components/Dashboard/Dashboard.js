f0085f3ca74a127e291f1b59cf7ff34db5a39952
abdur-rashid-khan <rashidkhan56925@gmail.com>
2022-08-18 11:44:12 +0600

    solve same problem

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -10,7 +10,7 @@ import {
 import "./Dashboard.css";
 import { Link, Outlet } from "react-router-dom";
 import { FaChartBar, FaChartLine } from "react-icons/fa";
-import "./Dashboard.css"
+// import "./Dashboard.css"
 import CustomLink from "../Share/CustomLink/CustomLink";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../Firebase/firebase.init";
@@ -27,56 +27,43 @@ const Dashboard = () => {
   }, [email]);
   return (
     <div className="mx-auto px-2 lg:px-0 route">
-      <div class="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content  flex flex-col" id="drawer">
+      <div className="drawer drawer-mobile pt-0">
+        <input id="open-dashboard-menu" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content  flex flex-col bg-slate-200">
           {/* <!-- Page content here --> */}
-          <div className="text-left mt-4 z-10">
-            <label
-              htmlFor="open-dashboard-menu"
-              className="rounded inline-block cursor-pointer text-white lg:hidden z-50"
-            >
-              <span className="text-2xl bg-green-400 hover:bg-green-500 px-4 py-2 rounded ">
-                <FontAwesomeIcon icon={faChevronRight} />
-              </span>{" "}
-            </label>
+          <div className="text-left mt-4 z-[1111111] fixed">
+            <label htmlFor="open-dashboard-menu" className="rounded inline-block cursor-pointer text-white lg:hidden z-50"><span className="text-2xl bg-gradient-to-r from-[#FF4402] to-[#f5ac1a] hover:bg-gradient-to-l from-[#e73f02] to-[#ffaa00] px-4 py-2 rounded shadow-2xl">
+              <FontAwesomeIcon icon={faChevronRight} /> </span></label>
           </div>
-          <div className="">
-            <div className="text-left -mt-12 lg:-mt-4 w-full">
-              {/* <div className="bg-[#0284C7] h-[500px] lg:-mr-3">
-                  <div className="pt-20 flex justify-center">
-                    <Count></Count>
-                  </div> */}
-
+          <div className="text-center ">
+            <div id="header" className='py-6'>
+            </div>
+            <div className="text-left px-3">
               <Outlet></Outlet>
-              {/* </div> */}
             </div>
           </div>
         </div>
-        <div class="drawer-side w-[180px]">
-          <label
-            htmlFor="open-dashboard-menu"
-            className="drawer-overlay"
-          ></label>
-          <ul className="menu overflow-y-auto  bg-[#1e293b] border-r-1 shadow-lg  ">
+        <div className="drawer-side w-[100%] lg:w-[250px]">
+          <label htmlFor="open-dashboard-menu" className="drawer-overlay "></label>
+          <ul className="menu overflow-y-auto  bg-[#1e293b] border-r-1 w-[70%] lg:w-[100%] border-r-1 shadow-lg  text-white">
             {/* <!-- Sidebar content here --> */}
             <div className="" id="sidebar_User_profile">
-              <div className="">
+              <div className="pt-5">
                 <div id="user_profile_photo">
-                  
-                  {currentUser?.image && (
-                          <img className="w-[75px] h-[75px] rounded-full m-auto" src={currentUser?.image} alt="" />
-                        )}
 
-                        {!currentUser?.image  && (
-                          <span className="">
-                            <AiOutlineUser className="w-[75px] h-[75px] border-2  text-slate-800 m-auto bg-white bg-opacity-50 text-4xl rounded-full" />
-                          </span>
-                        )}
+                  {currentUser?.image && (
+                    <img className="w-[75px] h-[75px] rounded-full m-auto" src={currentUser?.image} alt="" />
+                  )}
+
+                  {!currentUser?.image && (
+                    <span className="">
+                      <AiOutlineUser className="w-[75px] h-[75px] border-2  text-slate-800 m-auto bg-white bg-opacity-50 text-4xl rounded-full" />
+                    </span>
+                  )}
                 </div>
                 <div id="user_content" className="pt-2">
-                  <h1 className="text-white text-center text-sm">{currentUser?.displayName}</h1>
-                  <h1 className="text-white text-center text-xs">{currentUser?.email}</h1>
+                  <h1 className="text-white text-center text-sm capitalize">{currentUser?.displayName}</h1>
+                  <h1 className="text-white text-center text-sm">{currentUser?.email}</h1>
                 </div>
               </div>
             </div>
