319a0670aed2f97d038a3e86c9dba6c360d86e42
neyaznafiz <niiyazobug@gmail.com>
2022-08-22 02:08:57 +0600

    about us route animation done

--- src/Components/AboutUs/Special.js
+++ src/Components/AboutUs/Special.js
@@ -6,7 +6,10 @@ function Special() {
             style={{ backgroundImage: "url('https://i.ibb.co/DLQD5bZ/special-offer-bg.png')", backgroundSize: 'cover' }} >
 
             <div className='2xl:max-w-8xl mx-auto'>
-                <div className="flex justify-around py-14 sm:p-16">
+                <div className="flex justify-around py-14 sm:p-16"
+                 data-aos="fade-up"
+                 data-aos-duration="2000"
+                >
                     <div className='col-start-1 col-end-13 sm:col-start-1 sm:col-end-9 sm:px-4'>
                         <h1 className='text-3xl sm:text-4xl text-white px-10 sm:px-0 text-center sm:text-left'> Looking For Something <span className='font-semibold sm:font-bold'>Special For Your Moment?</span></h1>
                         <p style={{ wordSpacing: "4px" }} className='mt-3 text-stone-700 px-4 sm:px-0 text-center sm:text-left mb-5 sm:mb-0'>Contact us now and we will make your event unique & unforgettable</p>
