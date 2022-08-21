bbb455ed0f0b5d3bf9d9bcb18e7ad196d5d1a759
neyaznafiz <niiyazobug@gmail.com>
2022-08-21 23:01:34 +0600

    animation added in eventlisting section in home page

--- src/Components/Home/Offer/Offer.js
+++ src/Components/Home/Offer/Offer.js
@@ -4,8 +4,9 @@ const Offer = () => {
     return (
         <div className="bg-[url('/src/asset/special-offer-bg.png')] bg-cover">
             <div className='2xl:max-w-7xl mx-auto'
-                data-aos="flip-down"
-                data-aos-duration="2000">
+                data-aos="fade-up"
+                data-aos-duration="2000"
+                >
                 <div className="grid grid-cols-12 py-14 sm:p-16">
                     <div className='col-start-1 col-end-13 sm:col-start-1 sm:col-end-9 sm:px-4'>
                         <h1 className='text-3xl sm:text-4xl text-white px-10 sm:px-0 text-center sm:text-left'><span className='text-4xl sm:text-6xl text-amber-400 font-semibold'>30%</span> Off In June~July For <span className='font-semibold sm:font-bold'>Birthday Events</span></h1>
